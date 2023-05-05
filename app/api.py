0ddc34a6c234e461bc0628d4996d07a919720b0a
Jedrzej Runowicz <jedrzej.runowicz@gmail.com>
2023-05-05 18:42:13 +0200

    Features: Creating web page to sending messages

--- app/api.py
+++ app/api.py
@@ -1,5 +1,6 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from image import create_image
 from log import logger
@@ -7,15 +8,38 @@ from mailer import report_error, report_added_post
 
 app = FastAPI()
 
+origins = [
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
 
 @app.post("/")
 async def send_message(body: dict):
-    logger.info(f"Creating post with text: \n {body['message']}")
-    response = create_image(body["message"])
-    if response:
-        report_added_post(body["message"])
-    else:
-        report_error(response)
+    try:
+        logger.info(f"Creating post with text: \n {body['message']}")
+        response = create_image(body["message"])
+        if response:
+            report_added_post(body["message"])
+        else:
+            report_error(response)
+        return {
+            "status_code": 200,
+            "message": "Successfully added post"
+        }
+    except Exception as er:
+        logger.error(er)
+        return {
+            "status_code": 400,
+            "message": er
+        }
 
 
 if __name__ == "__main__":
