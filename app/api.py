43126542da9088a94a61e1ef0ccd90850b16ff3c
Jedrzej Runowicz <jedrzej.runowicz@gmail.com>
2023-05-05 22:41:46 +0200

    Fixes: fixes

--- app/api.py
+++ app/api.py
@@ -45,7 +45,7 @@ async def send_message(body: dict):
 if __name__ == "__main__":
     uvicorn.run(
         "api:app",
-        host="127.0.0.1",
+        host="0.0.0.0",
         reload=True,
         port=8000
     )
