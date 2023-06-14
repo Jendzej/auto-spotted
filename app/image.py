f50dd224d7e8d4eddd91807befa994447b8c51ad
Jedrzej Runowicz <jedrzej.runowicz@gmail.com>
2023-06-14 18:07:20 +0200

    Features: Added customizable background color

--- app/image.py
+++ app/image.py
@@ -56,12 +56,21 @@ def remove_old_images(current_image_number):
         pass
 
 
-def create_image(text, color):
+def create_image(text, color, background_color):
     """ Creating image with given text based on base.png """
+    base_color = {
+        "basic": "base.png",
+        "black": "base-black.png",
+        "white": "base-white.png",
+        "aqua": "base-aqua.png",
+        "brown": "base-brown.png",
+        "green": "base-green.png",
+        "pink": "base-pink.png"
+    }
     try:
         global posts_count
         posts_count += 1
-        img = Image.open(f"{os.getcwd()}/images/base.png")
+        img = Image.open(f"{os.getcwd()}/images/{base_color[background_color]}")
         font = ImageFont.truetype(f"{os.getcwd()}/fonts/NotoSansNandinagari-Regular.ttf", 50, encoding='utf-8')
 
         draw_multiple_line_text(img, text, font, color, 120)
